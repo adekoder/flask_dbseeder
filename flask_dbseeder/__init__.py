b485c32112622f3345a935c417517efb94876074
adewumi <adwumiogunbiyi@gmail.com>
2017-10-28 21:23:08 +0100

    code refactor

--- flask_dbseeder/__init__.py
+++ flask_dbseeder/__init__.py
@@ -1,5 +1,4 @@
 from flask import current_app
-import os
 from .seedmanager import SeedManager
 
 try:
@@ -9,19 +8,13 @@ except ImportError:
 
 class Seeder(object):
 
-    def __init__(self, app=None, *, db=None, directory=None):
-        self.directory = directory
-        if self.directory is None:
-            self.directory = 'Seeder'
-        if db is not None:
-            self.db = db
-        if app is not None:
+    def __init__(self, app=None, db=None):
+        if app is not None and db is not None:
             self.app = app
+            self.db = db
             self.init_app(self.app, self.db)
     
     def init_app(self, app, db):
-        self.root_path = app.__dict__['root_path']
-        self.seeder_path = os.path.join(self.root_path, self.directory)
         self.app = app
         if not hasattr(app, 'extensions'):
             app.extensions = {}
